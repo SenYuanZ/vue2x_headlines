1bcb4a97dedb7e8594284ee58287210ca70cfbcf
YuanTom <2274004370@qq.com>
2022-03-18 18:41:31 +0800

    完成了小智同学

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <!-- 路由的出口 -->
-    <router-view />
+    <!--
+    因为 router-view 是一级路由出口，
+    keep-alive 仅对该路由出口渲染的组件有效
+     -->
+    <keep-alive :include="['LayouIndex']">
+      <router-view />
+    </keep-alive>
   </div>
 </template>
 
